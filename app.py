cf0f49b6a5830e39168ff88d77f5b9ecaae081ff
Adrian Maziarz <adrian.maziarz.92@gmail.com>
2022-06-07 00:03:49 +0200

    change for test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "This is the place, where new service will be created. Please wait patiently!"
+    return "This is the place, where new service will be created. Please wait patiently!!!"
 
 if __name__ == "__main__":
     app.run()
